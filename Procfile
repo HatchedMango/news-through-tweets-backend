c02171df5e6c97e1dd228292bd86653083ea5d2a
StentorianCat <d1longfellow@gmail.com>
2017-12-11 17:20:29 -0600

    hopeful!

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn flask_rest_service:flask-api
+web: python ./flask_rest_service/flask-api.py
--- flask_rest_service/flask-api.py
+++ flask_rest_service/flask-api.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import os
 
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
@@ -73,4 +74,5 @@ class TwitterNewsData(Resource):
 API.add_resource(TwitterNewsData, '/news_data')
 
 if __name__ == '__main__':
-    APP.run(debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    APP.run(host='0.0.0.0', port=port)
