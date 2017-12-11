fe280ebe27fa8091d8cfc19e587cc312017dc8d0
StentorianCat <d1longfellow@gmail.com>
2017-12-11 17:56:01 -0600

    again

--- flask_rest_service/flask-api.py
+++ flask_rest_service/flask-api.py
@@ -75,4 +75,4 @@ API.add_resource(TwitterNewsData, '/news_data')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    APP.run(host='0.0.0.0', port=port)
+    APP.run(port=port)
